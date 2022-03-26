5a8a2419f6e36bf287d16b3b2e493f5737cbfe84
DavidLambertCyber <DavidLambertcyber@gmail.com>
2022-03-26 06:41:40 -0400

    minor

--- Invoke_Trader.py
+++ Invoke_Trader.py
@@ -2,7 +2,6 @@ import os,sys
 from textwrap import indent
 from etrade.etrade import ETrade
 # Remove-Item .\Invoke_Trader_py_Logs.txt ;cls; py.exe .\Invoke_Trader.py
-# Robocopy.exe "E:\trader" "D:\ONE_USB_LOCAL\_ws\py\etradeAPI\Invoke_Trader" /MIR
 
 from pprint import pprint
 
@@ -10,7 +9,7 @@ def main():
   et = ETrade(verbose=False,key_type='SANDBOX')
   # et = ETrade(verbose=False,key_type='LIVE')
   # et.get_quotes('BBW,TWO,F,GE,APA,TWNK,TTWO,BYND,CAT,GOOG,IBM,MRO,ACB,IDEX,BNGO,SNAP,NURO,RCON,RIG,BAC,TSLA,CGC,NIO,QS,SPWR,NEO,KMI')
-  pprint(et.get_quotes('BBW,TWO,F,GE'))
+  pprint(et.get_quotes('BBW,TWO,F,GE'),indent=2)
   et.revoke_token()
   
   
